c32cbb7e37579c53ed6bb3f5f45577fb1f831220
Glim App <appstore@glim.app>
2024-05-05 11:49:08 +0400

    Initial Commit

---
+++ Next Button/Next_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_ButtonApp.swift
+//  Next Button
+//
+//  Created by Marat Kharrasov on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Next_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
