bbaea98a2d789051525cff359ffdc58f4790261d
Glim App <appstore@glim.app>
2024-05-07 15:34:53 +0400

    bug fix

--- Next Button/ContentView.swift
+++ Next Button/ContentView.swift
@@ -8,35 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var isForward = false
+    @State var iconState = 0
+    @State private var timer: Timer?
     
     var body: some View {
         HStack {
             Button(action: {
-                withAnimation(.spring(response: 0.67, dampingFraction: 0.67, blendDuration: 0)) {
-                    isForward.toggle()
-                } completion: {
-                    isForward.toggle()
+                withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0)) {
+                    if iconState < 3 {
+                        iconState = iconState + 1
+                    } else {
+                        iconState = 0
+                    }
                 }
             }, label: {
-                HStack(spacing: -4) {
+                ZStack {
                     Image(systemName: "play.fill")
                         .foregroundStyle(.tint)
                         .font(.title)
-                        .scaleEffect(isForward ? 1 : 0, anchor: .trailing)
+                        .scaleEffect((iconState == 0 || iconState == 3) ? 0 : 1, anchor: (iconState == 0 || iconState == 1) ? .leading : .trailing)
+                        .offset(x: (iconState == 0 || iconState == 1) ? -10 : 10)
+                        .opacity((iconState == 0 || iconState == 3) ? 0 : 1)
                     Image(systemName: "play.fill")
                         .foregroundStyle(.tint)
                         .font(.title)
+                        .scaleEffect((iconState == 2 || iconState == 3) ? 0 : 1, anchor: (iconState == 0 || iconState == 3) ? .leading : .trailing)
+                        .offset(x: (iconState == 0 || iconState == 3) ? -10 : 10)
+                        .opacity((iconState == 2 || iconState == 3) ? 0 : 1)
                     Image(systemName: "play.fill")
                         .foregroundStyle(.tint)
                         .font(.title)
-                        .scaleEffect(isForward ? 0 : 1, anchor: .leading)
-                        .opacity(isForward ? 0 : 1)
-                }.padding(EdgeInsets(top: 0, leading: isForward ? 20 : -20, bottom: 0, trailing: 0))
+                        .scaleEffect((iconState == 2 || iconState == 1) ? 0 : 1, anchor: (iconState == 2 || iconState == 3) ? .leading : .trailing)
+                        .offset(x: (iconState == 2 || iconState == 3) ? -10 : 10)
+                        .opacity((iconState == 2 || iconState == 1) ? 0 : 1)
+                    Image(systemName: "play.fill")
+                        .foregroundStyle(.tint)
+                        .font(.title)
+                        .scaleEffect((iconState == 0 || iconState == 1) ? 0 : 1, anchor: (iconState == 1 || iconState == 2) ? .leading : .trailing)
+                        .offset(x: (iconState == 1 || iconState == 2) ? -10 : 10)
+                        .opacity((iconState == 0 || iconState == 1) ? 0 : 1)
+                }
             })
+            .onLongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity, pressing: { isPressing in
+                if isPressing {
+                    startTimer()
+                } else {
+                    stopTimer()
+                }
+            }, perform: {})
+            .buttonStyle(NoHighlightButtonStyle())
+            .clipShape(.circle)
         }
     }
+    private func startTimer() {
+            timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { _ in
+                withAnimation(.linear) {
+                    if iconState < 3 {
+                        iconState = iconState + 1
+                    } else {
+                        iconState = 0
+                    }
+                }
+            }
+        }
+
+        private func stopTimer() {
+            timer?.invalidate()
+            timer = nil
+        }
 }
 
 #Preview {
--- Next Button/PlayPauseView.swift
+++ Next Button/PlayPauseView.swift
@@ -11,29 +11,44 @@ struct PlayPauseView: View {
     @State private var animate = false
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .frame(width: 36, height: animate ? 36 : 12)
-                .foregroundColor(Color.blue)
-                .cornerRadius(4)
-                .offset(y: 10)
-            Rectangle()
-                .frame(width: 36, height: animate ? 36 : 12)
-                .foregroundColor(Color.blue)
-                .cornerRadius(4)
-                .offset(y: -10)
-        }
-        .mask {
-            Image(systemName: "play.fill")
-                .scaleEffect(animate ? 2 : 6)
-        }.rotationEffect(animate ? Angle(degrees: 0) : Angle(degrees: -90))
-        .onTapGesture {
-            withAnimation(.spring(response: 0.67, dampingFraction: 0.67, blendDuration: 0)){
+        Button(action: {
+            withAnimation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0)) {
                 animate.toggle()
             }
-        }
+        }, label: {
+            ZStack {
+                Group{
+                    Rectangle()
+                        .frame(width: 36, height: animate ? 36 : 12)
+                        .cornerRadius(4)
+                        .offset(y: 10)
+                    Rectangle()
+                        .frame(width: 36, height: animate ? 36 : 12)
+                        .cornerRadius(4)
+                        .offset(y: -10)
+                }
+            }
+            .mask {
+                Image(systemName: "play.fill")
+                    .scaleEffect(animate ? 2.25 : 6)
+            }.rotationEffect(animate ? Angle(degrees: 0) : Angle(degrees: 90))
+        })
+        .buttonStyle(NoHighlightButtonStyle())
     }
 }
+
+struct NoHighlightButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .frame(width: 84, height: 84)
+                .foregroundColor(.blue)
+                .background(configuration.isPressed ? Color.blue.opacity(0.1) : Color.clear)
+                .clipShape(Circle())
+                .scaleEffect(configuration.isPressed ? 0.8 : 1)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0), value: configuration.isPressed)
+    }
+}
+
 
 struct PlayPauseView_Previews: PreviewProvider {
     static var previews: some View {
